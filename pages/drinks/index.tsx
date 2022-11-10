0bec90b9e53f5bb471da368c35b0bad341d2bd04
Igor Dumchykov <Igor_Dumchykov@neimanmarcus.com>
2022-11-10 14:00:54 +0200

    Add docker support

--- pages/drinks/index.tsx
+++ pages/drinks/index.tsx
@@ -1,28 +1,43 @@
 import { BaseMenu } from "../../components/baseMenu/BaseMenu";
 import React, { useState } from "react";
 
-export default function Drinks() {
-  const [drinkMenu, setDrinkMenu] = useState({} as any)
 
-  function fetchDrinkMenuHandler() {
-    fetch("http://localhost:8080/api/v1/drinks")
-        .then((response) => response.json())
-        .then((data) => {
-          setDrinkMenu(data)
-        })
+export default function Drinks() {
+  const host = process.env.CORE_BASE_URL
+  console.log(`Host url: ${host}`)
+  const [drinkMenu, setDrinkMenu] = useState({} as any)
+  const [error, setError] = useState(null)
+
+  async function fetchDrinkMenuHandler() {
+    try {
+      const response = await fetch(`${host}:8080/api/v1/drinks`)
+      // const response = await fetch(`http://54.93.177.148:8080/api/v1/drinks`)
+
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      const data = await response.json()
+      setDrinkMenu(data)
+      setError(null)
+    } catch (err: any) {
+      console.log(err)
+      setError(err.message)
+    }
   }
 
   return <React.Fragment>
     <section>
-      <button onClick={fetchDrinkMenuHandler}>Get Drinks Menu</button>
+      <button onClick={fetchDrinkMenuHandler}>Drinks Menu</button>
     </section>
     <section>
-      <BaseMenu id={drinkMenu.id}
-                name={drinkMenu.name}
-                description={drinkMenu.description}
-                imageUrl={drinkMenu.imageUrl}
-                groups={drinkMenu.groups}>Drinks
-      </BaseMenu>
+      { !error && <BaseMenu id={drinkMenu.id}
+        name={drinkMenu.name}
+        description={drinkMenu.description}
+        imageUrl={drinkMenu.imageUrl}
+        groups={drinkMenu.groups}>Drinks
+        </BaseMenu>
+      }
+      {error && <p>{error}</p>}
     </section>
   </React.Fragment>
 
